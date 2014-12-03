2e6d6b8958db8679b989a0be7aec809a7f84d864
lexucuflexu <alex.tenche@gmail.com>
2014-12-03 12:28:28 +0200

    angular-directive not working

---
+++ server.js
@@ -0,0 +1,16 @@
+﻿
+var path = require('path'),
+	fs =  require('fs'),
+	http = require('http'),
+	express = require('express');
+	
+// ==================
+// Database
+// ==================
+
+var mongoose = 
+		require('mongoose')
+			.connect("mongodb://localhost:27017/angular-resource"),
+		db = mongoose.connection;
+		
+mongoose.set('debug', true);
