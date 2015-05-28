99fd5f41a23476aa6e01bc94b379070cdee38a96
Alex Tenche <alex.tenche@gmail.com>
2015-05-29 00:30:32 +0300

    finished dribbbleAPI

--- dirbbbleAPI/js/app.js
+++ dirbbbleAPI/js/app.js
@@ -1,3 +1,18 @@
 "use strict";
 
-var app = angular.module('dribble', ['dribble.controllers']);
+var app = angular.module('dribble', ['dribble.controllers', 'dribble.filters', 'dribbble.services']);
+
+app.config(function ($routeProvider){
+	$routeProvider
+	.when("/shots/:id", {
+		controller: "ShotsCtrl",
+		templateUrl: "partials/shot.html"
+	})
+	.when("/:list", {
+		controller: "ShotsListCtrl",
+		templateUrl: "partials/shots_list.html"
+	})
+	.otherwise({
+		redirectTo: "/popular"
+	});
+})
--- dirbbbleAPI/js/filters.js
+++ dirbbbleAPI/js/filters.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var filters = angular.module('dribble.filters', []);
+
+filters.filter('dribbbleDate', function ($filter) {
+
+	return function (value, format) {
+		console.log(value, format);
+		if(value){
+			value = Date.parse(value);
+		}
+		return $filter('date')(value, format);
+	}
+})
