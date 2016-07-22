29c2ec725053cfd2df969d6aa3425737846e5018
Alexandru Tenche <alex.tenche@gmail.com>
2016-07-22 10:32:37 +0300

    progress on todoTutorial

--- todoTutorial/js/app.js
+++ todoTutorial/js/app.js
@@ -1,1 +1,20 @@
-angular.module('todoListApp', []);
+angular.module('todoListApp', [])
+.controller('mainCtrl', function($scope, dataService){
+
+	$scope.helloConsole = dataService.helloConsole;
+	$scope.learn = function(){
+		console.log('input changed');
+	};
+
+	$scope.todos = [
+		{"name": "angularJS"},
+		{"name": "MEAN"},
+		{"name": "JavaScript Testing"},
+		{"name": "JavaScript track"}
+	];
+})
+.service('dataService', function(){
+	this.helloConsole = function(){
+		console.log("service working !");
+	};
+});
--- todoTutorial/js/hello.js
+++
@@ -1,7 +0,0 @@
-angular.module('todoListApp')
-.directive('hello', function(){
-	return {
-		template: "Hello to my little friend !",
-		restrict: "E"
-	};
-});
